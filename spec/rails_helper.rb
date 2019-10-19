4c4d18cf555bb3ffb5edff8c5c87b814e92ae879
Stella Mainar <mainars09@alumni.hanover.edu>
2019-10-19 17:07:45 -0600

    Require 'simplecov' and configure shoulda-matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,6 @@
+require 'simplecov'
+SimpleCov.start
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -60,4 +63,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end

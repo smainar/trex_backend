0d6ba2f608c06f2014a9017c29997c4407bc6eb7
Stella Mainar <mainars09@alumni.hanover.edu>
2019-10-20 17:23:27 -0600

    Add friendships to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
   enum role: ['wanderer', 'follower', 'admin']
+
+  has_many :friendships
+  has_many :friends, through: :friendships
+
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :inverse_friends, through: :inverse_friendships, source: :user
 end

67bc82cef9b5254cbf8d16b86e7394f92ce2cd2a
username <wnt925@gmail.com>
2019-10-21 12:41:27 -0600

    Update Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_20_032421) do
+ActiveRecord::Schema.define(version: 2019_10_20_032213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "itineraries", force: :cascade do |t|
+  create_table "destination", force: :cascade do |t|
+    t.bigint "leg_id"
+    t.string "city"
+    t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["leg_id"], name: "index_destination_on_leg_id"
   end
 
   create_table "legs", force: :cascade do |t|
@@ -25,19 +29,11 @@ ActiveRecord::Schema.define(version: 2019_10_20_032421) do
     t.string "name"
     t.string "start_date"
     t.string "end_date"
+    t.string "origin"
+    t.string "destination"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["trip_id"], name: "index_legs_on_trip_id"
-  end
-
-  create_table "locations", force: :cascade do |t|
-    t.bigint "leg_id"
-    t.string "city"
-    t.string "country"
-    t.string "emergency_phone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["leg_id"], name: "index_locations_on_leg_id"
   end
 
   create_table "trips", force: :cascade do |t|
@@ -52,6 +48,7 @@ ActiveRecord::Schema.define(version: 2019_10_20_032421) do
     t.string "name", null: false
     t.string "email", null: false
     t.integer "role"
+    t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

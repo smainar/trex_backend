c029030bb5e02593679625942084985aba14075b
username <wnt925@gmail.com>
2019-10-21 12:42:07 -0600

    Add leg type

--- app/graphql/types/leg_type.rb
+++ app/graphql/types/leg_type.rb
@@ -3,6 +3,8 @@ module Types
     field :name, String, null: false
     field :start_date, String, null: false
     field :end_date, String, null: false
+    field :origin, String, null: false
+    field :destination, String, null: false
 
   end
 end

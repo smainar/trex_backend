56969003165664534dcb881f18f87ef9a06bdbc8
username <wnt925@gmail.com>
2019-10-21 16:29:35 -0600

    Change migrations

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,22 @@ ActiveRecord::Schema.define(version: 2019_10_20_225121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "destination", force: :cascade do |t|
+  create_table "destinations", force: :cascade do |t|
     t.bigint "leg_id"
-    t.string "city"
-    t.string "country"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["leg_id"], name: "index_destination_on_leg_id"
+    t.index ["leg_id"], name: "index_destinations_on_leg_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true
+    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "legs", force: :cascade do |t|
@@ -30,7 +39,6 @@ ActiveRecord::Schema.define(version: 2019_10_20_225121) do
     t.string "start_date"
     t.string "end_date"
     t.string "origin"
-    t.string "destination"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["trip_id"], name: "index_legs_on_trip_id"
@@ -42,16 +50,6 @@ ActiveRecord::Schema.define(version: 2019_10_20_225121) do
     t.string "start_date"
     t.string "end_date"
     t.index ["user_id"], name: "index_trips_on_user_id"
-  end
-
-  create_table "friendships", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "friend_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["friend_id"], name: "index_friendships_on_friend_id"
-    t.index ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true
-    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|

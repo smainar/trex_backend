e94e4165758b3484c09a8d90462b4b470473dc5e
username <wnt925@gmail.com>
2019-10-21 12:39:57 -0600

    Create remove leg functionality

---
+++ app/graphql/mutations/remove_leg.rb
@@ -0,0 +1,13 @@
+module Mutations
+  class RemoveLeg < GraphQL::Schema::RelayClassicMutation
+
+    field :leg, Types::LegType, null: false
+    argument :id, ID, required: true
+
+    def resolve(id:)
+      leg = Leg.find(id)
+      leg.destroy!
+      { leg: leg }
+    end
+  end
+end

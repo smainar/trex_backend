55ef943507d4d427af360549da4ae588a0b1857d
Stella Mainar <mainars09@alumni.hanover.edu>
2019-10-22 17:36:33 -0600

    Add factory for Lodgings table

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,12 @@
 FactoryBot.define do
+  factory :lodging do
+    name { "MyString" }
+    arrival_date { "MyString" }
+    departure_date { "MyString" }
+    city { "MyString" }
+    destination { nil }
+  end
+
   factory :trip do
     sequence(:name) {"Spain" }
     sequence(:start_date) { "10/31/19" }

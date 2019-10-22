0bbf7e6731677b01da432fa99fae90219219a271
Stella Mainar <mainars09@alumni.hanover.edu>
2019-10-21 18:09:27 -0600

    Add Lodging model and test

---
+++ spec/models/lodging_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Lodging, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :arrival_date }
+    it { should validate_presence_of :departure_date }
+    it { should validate_presence_of :city }
+  end
+
+  describe 'relationships' do
+    it { should belong_to :destination }
+  end
+end

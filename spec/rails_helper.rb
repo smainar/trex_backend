a87522dc59af1b0ec220f14c77dee2fb8cd0d830
Stella Mainar <mainars09@alumni.hanover.edu>
2019-10-22 12:04:26 -0600

    Add line in rails_helper to use shortened syntax for building and creating objects using factories

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,7 +62,10 @@ RSpec.configure do |config|
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
+  # config.filter_gems_from_backtrace("gem name
+
+  # We are able to use shortened syntax for building and creating objects using factories.
+  config.include FactoryBot::Syntax::Methods
 end
 
 Shoulda::Matchers.configure do |config|

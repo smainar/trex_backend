c22bf50e05d06c6d1d21686bf18b3f3f9f1e9680
Stella Mainar <mainars09@alumni.hanover.edu>
2019-10-23 04:39:38 -0600

    Add UpdateLodging class to module Mutations

---
+++ app/graphql/mutations/update_lodging.rb
@@ -0,0 +1,36 @@
+module Mutations
+  class UpdateLodging < GraphQL::Schema::RelayClassicMutation
+    field :lodging, Types::LodgingType, null: false
+    field :errors, [String], null: false
+
+    argument :id, ID, required: true
+    argument :name, String, required: false
+    argument :arrival_date, String, required: false
+    argument :departure_date, String, required: false
+    argument :city, String, required: false
+    argument :destination_id, Integer, required: false
+
+    def resolve(id:, name: nil, arrival_date: nil, departure_date: nil, city: nil, destination_id: nil)
+      lodging = Lodging.find(id)
+
+      params = (
+        name: name,
+        arrival_date: arrival_date,
+        departure_date: departure_date,
+        city: city,
+        destination_id: destination_id
+      )
+
+      if lodging.update(params)
+        {
+          lodging: lodging ,
+          errors: [],
+        }
+      else
+        {
+          errors: lodging.errors.full_messages
+        }
+      end
+    end
+  end
+end

ce8fcd9e63387a159652ab7512ea07cbc382bca9
Stella Mainar <mainars09@alumni.hanover.edu>
2019-10-23 07:11:55 -0600

    Add RemoveTransportation class in module Mutations

---
+++ app/graphql/mutations/remove_transportation.rb
@@ -0,0 +1,13 @@
+module Mutations
+  class RemoveTransportation < GraphQL::Schema::RelayClassicMutation
+
+    field :transportation, Types::TransportationType, null: false
+    argument :id, ID, required: true
+
+    def resolve(id:)
+      transportation = Transportation.find(id)
+      transportation.destroy!
+      { transportation: transportation }
+    end
+  end
+end

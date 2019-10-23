314319066d999fe9c3e07f5b328b561188bf3a22
Stella Mainar <mainars09@alumni.hanover.edu>
2019-10-23 05:28:31 -0600

    Add fields to TransportationType class

---
+++ app/graphql/types/transportation_type.rb
@@ -0,0 +1,10 @@
+module Types
+  class TransportationType < Types::BaseObject
+    field :id, ID, null: false
+    field :departure_time, String, null: false
+    field :arrival_time, String, null: false
+    field :departure_city, String, null: false
+    field :arrival_city, String, null: false
+    field :leg_id, Integer, null: false
+  end
+end

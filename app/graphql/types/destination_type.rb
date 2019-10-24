62ac179ff40ec5e6aef7f808011e252c0ca5179c
Stella Mainar <mainars09@alumni.hanover.edu>
2019-10-23 18:42:08 -0600

    Add has_many relationship between destination and lodgings in DestinationType class

--- app/graphql/types/destination_type.rb
+++ app/graphql/types/destination_type.rb
@@ -3,5 +3,6 @@ module Types
     field :id, ID, null: false
     field :name, String, null: false
     field :leg_id, Integer, null: false
+    field :lodgings, [Types::LodgingType], null: true
   end
 end

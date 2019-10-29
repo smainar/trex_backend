e945cdbc705c7e314289afb3cdcbc092e88afb41
Stella Mainar <mainars09@alumni.hanover.edu>
2019-10-29 17:44:04 -0600

    Add updated schema after migration to add phone_number column to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_151951) do
+ActiveRecord::Schema.define(version: 2019_10_29_212445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -175,6 +175,7 @@ ActiveRecord::Schema.define(version: 2019_10_29_151951) do
     t.integer "role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number"
   end
 
   add_foreign_key "currency_informations", "destinations"

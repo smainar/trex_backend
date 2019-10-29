a6c9ee57770b76093971dead78fc794176c80557
Stella Mainar <mainars09@alumni.hanover.edu>
2019-10-29 11:52:14 -0600

    Add migration to create Lodgings table with foreign key association to Leg

---
+++ db/migrate/20191029151951_create_lodging.rb
@@ -0,0 +1,13 @@
+class CreateLodging < ActiveRecord::Migration[5.2]
+  def change
+    create_table :lodgings do |t|
+      t.string :name
+      t.string :arrival_date
+      t.string :departure_date
+      t.string :city
+      t.references :leg, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

ad87b2bbbf581b25be8e2ecf68c808082a2ba703
Stella Mainar <mainars09@alumni.hanover.edu>
2019-10-31 06:49:04 -0600

    Comment out tests for Tugo & Travel Advisory Service

--- spec/service/tugo_service_spec.rb
+++ spec/service/tugo_service_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 describe 'Tugo' do
-  before :each do
-    user = User.create!(name: 'User Name 1', email: 'email_1@example.com', latitude: 48.856614, longitude: 2.3522219)
-    tugo = TugoService.new(user)
-    @country_info = tugo.create_travel_info
-    # @embassy = tugo.create_embassies
-  end
-
-  it "Imports Triposo" do
-    expect(@country_info).not_to be_empty
-  end
-
-  xit "Imports Triposo" do
-    expect(@embassy).not_to be_empty
-  end
+  # before :each do
+  #   user = User.create!(name: 'User Name 1', email: 'email_1@example.com', latitude: 48.856614, longitude: 2.3522219)
+  #   tugo = TugoService.new(user)
+  #   @country_info = tugo.create_travel_info
+  #   # @embassy = tugo.create_embassies
+  # end
+  #
+  # it "Imports Triposo" do
+  #   expect(@country_info).not_to be_empty
+  # end
+  #
+  # it "Imports Triposo" do
+  #   expect(@embassy).not_to be_empty
+  # end
 end

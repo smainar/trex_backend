66e916d6010ba5996fed30f4f76042adbb132409
Stella Mainar <mainars09@alumni.hanover.edu>
2019-10-31 06:46:59 -0600

    Delete whitespace on line 4

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,4 @@
 class Notification < ApplicationRecord
   belongs_to :sender, class_name: "User", foreign_key: "sender_id"
   belongs_to :receiver, class_name: "User", foreign_key: "receiver_id"
-
  end
